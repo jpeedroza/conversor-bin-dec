dc516c350e639a014bebeac0d76586ad1eab8036
JoaoMac <jplima.pedroza@gmail.com>
2020-10-15 10:56:27 -0300

    First Commit

--- conversor.js
+++ conversor.js
@@ -1,26 +1,29 @@
-function conversorDec() {
+  function conversorDec() {
   let valorDec = document.querySelector('#decNum').value;
   var convertDec = (valorDec >>> 0).toString(2);
 
   if(valorDec === '') {
-    return alert('Insira um número');
+    return alert('Insert a number');
   }
 
   document.querySelector('#binNum').value = convertDec;
   return true;
 }
 function conversorBin() {
+  document.querySelector('#decNum1').value = ''
   let valorBin = document.querySelector('#binNum1').value;
   var convertBin = parseInt(valorBin, 2).toString(10);
 
   if(valorBin === '') {
-    return alert('Insira um número');
+    return alert('Insert a number');
     }
-  valorBin.split('').map((char) => {
-    if(char !== '0' && char !== '1'){
-      return alert('Insira um número binário')};
-  });
-
-  document.querySelector('#decNum1').value = convertBin;
-  return true;
+    valorBin.split(' ').map((value) => {
+      if (/[^01]/.test(value)) {
+        document.querySelector('#decNum1').value = "Non binary";
+        return alert("This isn't a binary number")
+      } else {
+        document.querySelector('#decNum1').value = convertBin;
+        return true;
+      }
+    });
 }
